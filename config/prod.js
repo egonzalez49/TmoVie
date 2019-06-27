11f6c93ab9f554817d9a78af514c3c1fa97476a6
egonzalez49 <egonzalez49@gatech.edu>
2019-06-26 20:15:06 -0400

    dev

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  twitterClientId: process.env.TWITTER_CLIENT_ID,
+  twitterClientSecret: process.env.TWITTER_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  tmdbKey: process.env.TMDB_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN
+};
